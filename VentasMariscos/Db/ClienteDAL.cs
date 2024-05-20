90c6a398e49cd6762e8e285175d3877bd0085502
imorales502 <amoralesdro97@gmail.com>
2024-05-20 01:55:52 -0600

    Se agrego entidad cliente

---
+++ VentasMariscos/Db/ClienteDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace VentasMariscos.Db
+{
+    public class ClienteDAL:CadenaDb
+    {
+        public DataTable ValidaCliente(string Nit, string Nombre, string Direccion)
+        {
+            DataTable tbl = new DataTable();
+            using(SqlConnection cn = new SqlConnection(CadenaConexion))
+            {
+                try
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SPValidaCliente", cn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("PNit", Nit);
+                        cmd.Parameters.AddWithValue("PNombre", Nombre);
+                        cmd.Parameters.AddWithValue("PDireccion", Direccion);
+                        SqlDataReader rd = cmd.ExecuteReader();
+                        tbl.Load(rd);
+                    }
+
+                    cn.Close();
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+            }
+
+            return tbl;
+        }
+    }
+}
---
+++ VentasMariscos/Entidad/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using VentasMariscos.Db;
+
+namespace VentasMariscos.Entidad
+{
+    public class Cliente
+    {
+        public static int ID { get; set; }
+        public string Nit { get; set; }
+        public string Nombre { get; set; }
+        public string Direccíon { get; set; }
+
+    }
+}
